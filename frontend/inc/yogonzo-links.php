b99ad60fed40f5f3b455747096a5aa6090c73f6d
KNE <divumpower@gmail.com>
2015-06-18 01:08:29 +0100

    favicon text link update

--- frontend/inc/yogonzo-links.php
+++ frontend/inc/yogonzo-links.php
@@ -5,4 +5,4 @@
 <link rel="stylesheet" type="text/css" href="frontend/libs/js/Bootstrap-Image-Gallery-3.1.1/css/blueimp-gallery.min.css">
 <link rel="stylesheet" type="text/css" href="frontend/libs/js/Bootstrap-Image-Gallery-3.1.1/css/bootstrap-image-gallery.min.css">
 <link rel="stylesheet" type="text/css" href="frontend/css/style.css">
-<link rel="icon" type="image/png" href="frontend/images/yogonzo_icon1.png">
+<link rel="icon" type="image/png" href="frontend/images/yogonzo-logo.ico">
