95f2431687973bbcfa9b95557d7228066cf06dc4
KNE <divumpower@gmail.com>
2015-06-18 01:19:17 +0100

    kill right click

--- frontend/scripts/yogonzo.js
+++ frontend/scripts/yogonzo.js
@@ -1,4 +1,13 @@
 // alert("hello world");
+var message="Function not available";
+    ///////////////////////////////////
+    function clickIE4(){if (event.button==2){alert(message);return false;}}
+    function clickNS4(e){if (document.layers||document.getElementById&&!document.all)
+    {if (e.which==2||e.which==3){alert(message);return false;}}}if (document.layers)
+    {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}
+    else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}
+    document.oncontextmenu=new Function("alert(message);return false")
+
 $(document).ready(function () {
 
     $(window).scroll(function(){
@@ -149,7 +158,6 @@ $(document).ready(function () {
         $('.sneaky-button').fadeOut(500);
         $('.auth-button').fadeIn();
     });
-
 
 });
     
